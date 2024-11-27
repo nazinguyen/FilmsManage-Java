5df678ca5a0f760a5e810caf362ab9cd4742382b
Nhan Thanh Pham <nhanpham.04122004@gmail.com>
2024-11-27 19:07:29 +0700

    Add Account

--- FilmsManage/FilmsManage/Models/TaiKhoan.cs
+++ FilmsManage/FilmsManage/Models/TaiKhoan.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmsAPI.Models;
 
 public partial class TaiKhoan
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int MaTaiKhoan { get; set; }
 
     public int MaKh { get; set; }
